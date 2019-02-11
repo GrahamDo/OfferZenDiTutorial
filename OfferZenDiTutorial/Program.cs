ec10d25a419092f42163e49de629b67099257a72
Graham Downs <>
2019-02-11 16:00:56 +0200

    Version 1 (Before logging)

--- OfferZenDiTutorial/Program.cs
+++ OfferZenDiTutorial/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OfferZenDiTutorial
 {
@@ -10,12 +7,67 @@ namespace OfferZenDiTutorial
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+            var number1 = GetNumber("Enter the first number: > ");
+            var number2 = GetNumber("Enter the second number: > ");
+            var operand = GetOperand();
+            var calc = new Calculator();
+            var result = GetResult(calc, number1, number2, operand);
+            Console.WriteLine($"{number1} {operand} {number2} = {result}");
+            Console.Write("Press any key to continue...");
             Console.ReadKey();
+        }
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        private static float GetNumber(string message)
+        {
+            var isValid = false;
+            while (!isValid)
+            {
+                Console.Write(message);
+                var input = Console.ReadLine();
+                isValid = float.TryParse(input, out var number);
+                if (isValid)
+                    return number;
+
+                Console.WriteLine("Please enter a valid number. Press ^C to quit.");
+            }
+
+            return -1;
+        }
+
+        private static char GetOperand()
+        {
+            var isValid = false;
+            while (!isValid)
+            {
+                Console.Write("Please type the operand (/*+-) > ");
+                var input = Console.ReadKey();
+                Console.WriteLine();
+                var operand = input.KeyChar;
+                if ("/*+-".Contains(operand))
+                {
+                    isValid = true;
+                    return operand;
+                }
+
+                Console.WriteLine("Please enter a valid operand (/, *, +, or -). " +
+                                  "Press ^C to quit.");
+            }
+
+            return ' ';
+        }
+
+        private static float GetResult(Calculator calc, float number1, float number2, char operand)
+        {
+            switch (operand)
+            {
+                case '/': return calc.Divide(number1, number2);
+                case '*': return calc.Multiply(number1, number2);
+                case '+': return calc.Add(number1, number2);
+                case '-': return calc.Subtract(number1, number2);
+                default:
+                    // Should never happen if our previous validations are working
+                    throw new InvalidOperationException($"Invalid operand passed: {operand}");
+            }
         }
     }
 }

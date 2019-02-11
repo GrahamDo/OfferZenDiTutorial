57f700d62a25c622639fe7e08fb59f8f719354e7
Graham Downs <>
2019-02-11 16:04:55 +0200

    Version 2 (Embedded File Logging)

--- OfferZenDiTutorial/Calculator.cs
+++ OfferZenDiTutorial/Calculator.cs
@@ -1,24 +1,32 @@
-﻿namespace OfferZenDiTutorial
+﻿using System.IO;
+
+namespace OfferZenDiTutorial
 {
     public class Calculator
     {
+        private const string FileName = "Calculator.log";
+
         public float Divide(float number1, float number2)
         {
+            File.WriteAllText(FileName, $"Running {number1} / {number2}");
             return number1 / number2;
         }
 
         public float Multiply(float number1, float number2)
         {
+            File.WriteAllText(FileName, $"Running {number1} * {number2}");
             return number1 * number2;
         }
 
         public float Add(float number1, float number2)
         {
+            File.WriteAllText(FileName, $"Running {number1} + {number2}");
             return number1 + number2;
         }
 
         public float Subtract(float number1, float number2)
         {
+            File.WriteAllText(FileName, $"Running {number1} - {number2}");
             return number1 - number2;
         }
     }
